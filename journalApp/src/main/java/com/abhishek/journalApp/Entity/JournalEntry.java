a754652af2d9a6763b828f02199b4015cf29e130
Abhishek Raj <abhishekraj605912@gmail.com>
2025-08-03 21:43:47 +0530

    Added Date variable and changed id dataType

--- journalApp/src/main/java/com/abhishek/journalApp/Entity/JournalEntry.java
+++ journalApp/src/main/java/com/abhishek/journalApp/Entity/JournalEntry.java
@@ -1,29 +1,50 @@
 package com.abhishek.journalApp.Entity;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document(collection = "journal_entries")
 public class JournalEntry {
 
-    private long id ;
+    @Id
+    private ObjectId id ;
+
     private String title;
+
     private String content;
 
-    public long getId() {
+    private LocalDateTime date;
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public ObjectId getId() {
         return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getContent() {
+        return content;
     }
 
     public void setContent(String content) {
