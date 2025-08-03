3f433c4bf91647aca6e688eba51e4ca32f668678
Abhishek Raj <abhishekraj605912@gmail.com>
2025-08-03 21:59:23 +0530

    Added repo interface

---
+++ journalApp/src/main/java/com/abhishek/journalApp/repository/JournalEntryRepo.java
@@ -0,0 +1,10 @@
+package com.abhishek.journalApp.repository;
+
+
+import com.abhishek.journalApp.Entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepo extends MongoRepository<JournalEntry, ObjectId> {
+
+}
